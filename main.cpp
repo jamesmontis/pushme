0ff3ae45b5b5e9801435282dbeca3f7d80e90d7a
J M <jmontis@gmail.com>
2020-12-06 21:18:49 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+// sample push after editing
+// fakeOOP repository for github practice
+//
+// https://github.com/jamesmontis/fakeOOP
+//
+// Welcome to Learn GitHub 101
+
+// Here we will practice IDE use and also command line
